dfe003fa4904d362dfd97b4208e0bfdb70c9913f
Mike Panuelos <60069774+snipergz@users.noreply.github.com>
2022-03-13 15:05:39 -0500

    uploaded files manually

---
+++ home.js
@@ -0,0 +1,20 @@
+// const { get } = require("http");
+const button = document.getElementById('play-button');
+const link_box = document.getElementById('link-box');
+button.addEventListener('click', async () => 
+{
+    const url = link_box.value;
+    console.log(url);
+    const response = await fetch(url, {
+        credentials: 'include',
+        method: 'GET',
+        headers: {'content-type': 'application/json'},
+        mode: "no-cors",
+        body: JSON.stringify()
+        // referrerPolicy: 'strict-origin-when-cross-origin'
+    });
+    // const data = await response.json();
+    // console.info('fetch()', response);
+    // console.log(data);
+    console.log(response);
+});
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const path = require('path');
+const PORT=8080;
+const static_dir = path.join(__dirname, 'static');
+
+// const sqlite3 = require('sqlite3');
+// const { open } = require('sqlite');
+// const req = require('express/lib/request');
+
+console.log(`Directory is ${static_dir}`);
+
+
+/////////////////////////////////////////////
+// express and middleware setup
+
+const app = express();
+app.use(express.static(static_dir));
+app.use(express.urlencoded({extended: false})); 
+app.set('view engine', 'ejs');
+
+// serve static files
+app.use(express.static(path.join(__dirname, 'static')));
+
+/////////////////////////////////////////////
+// routes
+
+app.get('/home', async (req, res) =>{
+	console.log("At home page");
+	res.render('home');
+});
+
+app.get('/play', async (req, res) =>{
+	console.log("Person is now playing");
+	// questions = ["What is a dog", "Where is the fish", "Are we there ye"];
+	async function scrapeProduct(url){
+		console.log("Scraping Browser Started...");
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(url);
+		console.log("Scraping in progress...");
+	 	wordText = await page.$$eval('a.SetPageTerm-wordText > span.TermText', el => el.map(el => el.textContent));
+		console.log("Got the questions...");
+		// defText = await page.$$eval('a.SetPageTerm-definitionText > span.TermText', el => el.map(el => el.textContent));
+		console.log(wordText);
+		// console.log(defText);
+		console.log("Finished Scraping...");
+		browser.close();
+		return wordText;
+	}
+	
+	const questions = await scrapeProduct('https://quizlet.com/675591396/se1-midterm-flash-cards/')
+	res.render('play', {questions: questions});
+})
+
+
+
+/////////////////////////////////////////////
+// start up server
+
+console.log("Javascript running on the server");
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
