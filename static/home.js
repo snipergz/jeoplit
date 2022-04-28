7a7a0754206c0b6bbed6079c05de16027abbeb31
Mike Panuelos <mikepanuelos@Mikes-MacBook-Air.local>
2022-04-28 11:34:09 -0500

    Added a very simple loading section

--- static/home.js
+++ static/home.js
@@ -1,11 +1,20 @@
 // const res = require("express/lib/response");
 const body = document.getElementsByTagName('body');
 const err = document.getElementById('error_msg');
+const notice = document.getElementById('notice');
+const playForm = document.getElementById('playForm');
+const practice = document.getElementById('practice');
 
 const showError = () => {
     err.classList.remove('d-none'); 
     err.innerText = "It still hasn't loaded";
 };
+
+practice.addEventListener('click', () => {
+    console.log("Btn Clicked")
+    notice.classList.remove('d-none');
+    playForm.classList.add('d-none');
+})
 
 // const { get } = require("http");
 const button = document.getElementById('play-button');
@@ -13,14 +22,6 @@ const link_box = document.getElementById('link-box');
 button.addEventListener('click', async () => 
 {
     console.log("Btn Clicked");
-    // let url = link_box.value;
-    // let response = "";
-    // let encURIComp = encodeURIComponent(`${url}`);
-	// console.log(encURIComp);
-    // console.log(url);
-    // while(!response.ok){
-    //     showError();
-    // }
-    // const data = await response.json();
-    // console.log(data);
+    notice.classList.remove('d-none');
+    playForm.classList.add('d-none');
 });
