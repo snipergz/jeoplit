4a9f9b22d8c6e3c967958782cb8d7cfa39274625
Mike Panuelos <mikepanuelos@Mikes-MacBook-Air.local>
2022-04-29 16:50:26 -0500

    loading screen only shows after play is clicked or form is submitted, loading screen is still unresponsive

--- static/home.js
+++ static/home.js
@@ -1,24 +1,9 @@
 // const res = require("express/lib/response");
 const body = document.getElementsByTagName('body');
-const err = document.getElementById('error_msg');
 const notice = document.getElementById('notice');
 const playForm = document.getElementById('playForm');
-const practice = document.getElementById('practice');
-
-const showError = () => {
-    err.classList.remove('d-none'); 
-    err.innerText = "It still hasn't loaded";
-};
-
-practice.addEventListener('click', () => {
-    console.log("Btn Clicked")
-    notice.classList.remove('d-none');
-    playForm.classList.add('d-none');
-})
-
-// const { get } = require("http");
 const button = document.getElementById('play-button');
-const link_box = document.getElementById('link-box');
+
 button.addEventListener('click', async () => 
 {
     console.log("Btn Clicked");
