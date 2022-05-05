6e48cb5c9dc495b69a9679a26a6503132c0d95e9
Mike Panuelos <mikepanuelos@Mikes-MacBook-Air.local>
2022-05-05 10:43:48 -0500

    Updated testing check's design to be responsive

--- static/TESTINGCHECK.js
+++ static/TESTINGCHECK.js
@@ -38,9 +38,57 @@ document.querySelector('#flipQA').addEventListener('click', function() {
     })());
 })
 
+document.querySelector('#flipQAPhone').addEventListener('click', function() {
+    let questions = document.querySelectorAll('.questionPhone');
+    let answers = document.querySelectorAll('.answerPhone');
+
+    let questionArr = [];
+    let answerArr = [];
+
+    questions.forEach(question => questionArr.push(question.innerText));
+    answers.forEach(answer => answerArr.push(answer.innerText));
+
+    //console.log(questionArr);
+    //console.log(answerArr);
+
+    let i = 0;
+    questions.forEach(question => (function() {
+        question.innerText = answerArr[i];
+        i++;
+    })());
+
+    i = 0;
+    answers.forEach(answer => (function() {
+        answer.innerText = questionArr[i];
+        i++;
+    })());
+})
+
 document.querySelector('#confirmationButton').addEventListener('click', async function() {
     let questions = document.querySelectorAll('.question');
     let answers = document.querySelectorAll('.answer');
+
+    let questionArr = [];
+    let answerArr = [];
+
+    questions.forEach(question => questionArr.push(question.innerText));
+    answers.forEach(answer => answerArr.push(answer.innerText));
+
+    await fetch('/testingWithNewSet', {
+        headers: {'Content-Type': 'application/json'},
+        method: 'POST',
+        body: JSON.stringify({questions: questionArr, answers: answerArr})
+    }).then(async (res) => {
+        window.location.assign('/testingPlay');
+    }).catch(async (res) => {
+        window.location.assign('/returnHome');
+    })
+
+}); 
+
+document.querySelector('#confirmationButtonPhone').addEventListener('click', async function() {
+    let questions = document.querySelectorAll('.questionPhone');
+    let answers = document.querySelectorAll('.answerPhone');
 
     let questionArr = [];
     let answerArr = [];
